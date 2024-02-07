b69f3c5e280f3f88a6b2c248b62d67afee8b12df
Victor Mjimapemba <vcmjimapemba@gmail.com>
2024-02-07 16:41:32 +0200

    Create django_apis.py

---
+++ django_apis.py
@@ -0,0 +1,62 @@
+#make sure you have all relevant imports
+#do this stuff in your api app
+
+#serializers.py
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = '__all__'
+
+
+#views.py
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+
+class PropertyViewSet(viewsets.ModelViewSet):
+    serializer_class = PropertySerializer
+    queryset = Property.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        # Filter properties based on the user's entity_id
+        properties = self.queryset.filter(entity_id=request.user.entity_id)
+        serializer = self.get_serializer(properties, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        # Explicitly check for 'entity_id' in request data
+        if 'entity_id' not in request.data:
+            return Response({"error": "'entity_id' is required in the request data"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#urls.py
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'properties', PropertyViewSet, basename='property')
+
+urlpatterns = [
+    path('', include(router.urls)),
